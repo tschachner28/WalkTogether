2d5dfb3c75ed955b2d823816b584b4015ae32039
tschachner28 <53120075+tschachner28@users.noreply.github.com>
2021-03-19 18:16:52 -0400

    matched based on time and location; bug fixes; styling

--- webapp/models.py
+++ webapp/models.py
@@ -8,6 +8,7 @@ class Volunteer(models.Model):
     #phone_number = models.IntegerField(default=-1)
     times_available = models.TextField(max_length=300)  # format: start_datetime1, end_datetime1; start_datetime2, end_datetime2; etc.
     phone_number = models.IntegerField()
+    walk_shifts = models.TextField(default='') # list of info for people this volunteer will be walking. Format: first_name1, last_name1, phone_number1, pickup_datetime1, pickup_location1; first_name2...
 
     def __str__(self):  # overrides the string method
         return str(self.first_name + ' ' + self.last_name)
--- webapp/searchpage.py
+++ webapp/searchpage.py
@@ -1,5 +1,5 @@
 from geopy import distance, Nominatim
-from datetime import datetime
+import datetime
 from django.shortcuts import render
 from django import forms
 from . import models
@@ -9,6 +9,7 @@ from django.contrib.sessions.models import Session
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
 import urllib.parse
+from django.utils.html import strip_tags
 
 
 class SignupForm(forms.Form):
@@ -18,7 +19,7 @@ class SignupForm(forms.Form):
     first_name = forms.CharField(max_length=20, label='First Name')
     last_name = forms.CharField(max_length=20, label='Last Name')
     phone_number = forms.IntegerField(label='Phone Number')
-    pickup_time = forms.DateTimeField(label='Pickup Time')
+    pickup_time = forms.DateTimeField(label='Pickup Date/Time')
     pickup_loc = forms.CharField(max_length=100, label='Pickup Location')
 
 
@@ -26,23 +27,50 @@ def submission(request):
     submitted = False
     if request.method == 'POST':
         form = SignupForm(request.POST)
-        volunteer_match = None
+        volunteer_match = 'none'
         cd = None
         if form.is_valid():
             #form.save()
             cd = form.cleaned_data
             all_volunteers = Volunteer.objects.all()
-
-            print(all_volunteers)
+            #print(all_volunteers)
 
             for vol in all_volunteers:
-                if calc_distance(cd['pickup_loc'], vol.location) <= 10: # match with volunteer located <= 10 miles away
-                    volunteer_match = vol
-                    break
+                print(cd['pickup_loc'])
+                print(cd['pickup_time'])
+                if calc_distance(cd['pickup_loc'], vol.location) <= 10: # match with volunteer who is located <= 10 miles away and available during the specified time
+                    vol_times_available = vol.times_available.split(' ')
+                    print(vol_times_available)
+                    i = 0
+                    while i < len(vol_times_available)-3:
+                        print("start: " + str(datetime.datetime.strptime(strip_tags(vol_times_available[i]) + ' ' + strip_tags(vol_times_available[i+1][0:-1]), '%Y-%m-%d %H:%M:%S')))
+                        print("end: " + str(datetime.datetime.strptime(
+                            strip_tags(vol_times_available[i+2]) + ' ' + strip_tags(vol_times_available[i + 3])[0:-1],
+                            '%Y-%m-%d %H:%M:%S')))
+
+                        start_time = datetime.datetime.strptime(strip_tags(vol_times_available[i]) + ' ' + strip_tags(vol_times_available[i+1][0:-1]), '%Y-%m-%d %H:%M:%S')
+                        end_time = datetime.datetime.strptime(strip_tags(vol_times_available[i+2]) + ' ' + strip_tags(vol_times_available[i + 3])[0:-1],
+                            '%Y-%m-%d %H:%M:%S')
+                        print("start_time: " + str(start_time))
+                        print("end_time: " + str(end_time))
+                        if start_time <= cd['pickup_time'].replace(tzinfo=None) <= end_time:
+                            volunteer_match = vol
+                            # Update volunteer model to reflect match
+                            volunteer_match.walk_shifts += cd['first_name'] + ', ' + cd['last_name'] + ', ' + str(
+                                cd['phone_number']) + ', ' + str(cd['pickup_time']) + ', ' + cd['pickup_loc'] + '; '
+                            volunteer_match.save()
+                            break
+                        else:
+                            i += 4
+
+                    #break
 
         #return HttpResponseRedirect('/?submitted=True')
-        vol_id = str(volunteer_match.id) if volunteer_match != None else 'none'
-        vol_name = str(volunteer_match.first_name)if volunteer_match != None else 'none'
+
+
+        # Print volunteer match (or lack thereof) on redirected page
+        vol_id = str(volunteer_match.id) if volunteer_match != 'none' else 'none'
+        vol_name = str(volunteer_match.first_name)if volunteer_match != 'none' else 'none'
         #return HttpResponseRedirect('/?submitted=True&vid=' + vol_id + '&vname=' + vol_name + '&loc=' + cd['pickup_loc'] + '&time=' + cd['pickup_time'])
         query = {"vid": vol_id, "vname": vol_name, "pickuploc": cd['pickup_loc'], "pickuptime": cd['pickup_time']}
         return HttpResponseRedirect('/?submitted=True&' + urllib.parse.urlencode(query))
