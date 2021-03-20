986ff785a10075a9f0cb98ac497c54965d2c86c7
tschachner28 <53120075+tschachner28@users.noreply.github.com>
2021-03-20 18:29:44 -0400

    final tweaks

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from .models import Volunteer
+from django.conf import settings
+from django.http import HttpResponse
+from twilio.rest import Client
 
 def searchpage(request):
     #return render(request=request, template_name='searchpage.html', context={"searchpage": searchpage})
     return render(request=request, template_name='searchpage.html', context={"Volunteer": Volunteer.objects.all})
+
+
+def broadcast_sms(request):
+    message_to_broadcast = ("Have you played the incredible TwilioQuest "
+                                                "yet? Grab it here: https://www.twilio.com/quest")
+    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    for recipient in settings.SMS_BROADCAST_TO_NUMBERS:
+        if recipient:
+            client.messages.create(to=recipient,
+                                   from_=settings.TWILIO_NUMBER,
+                                   body=message_to_broadcast)
+    return HttpResponse("messages sent!", 200)
 
